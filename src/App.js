b4c25fda81026ece2669d03242b19d82d8e47aca
Brittney <brittney.postma@hotmail.com>
2020-08-01 21:38:01 -0400

    materials

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, useThree, extend } from 'react-three-fiber'
+import { Canvas, useThree, extend, useFrame } from 'react-three-fiber'
+import { a, useSpring } from 'react-spring/three'
 import './App.css';
 
 extend({ OrbitControls })
 
 function Cube(props) {
+  const ref = useRef()
   const [isBig, setIsBig] = useState(false)
   const [isHovered, setIsHovered] = useState(false)
 
-  const size = isBig ? 2 : 1
+  useFrame(() => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+
+  })
+
+  const { size, x } = useSpring({
+    size: isBig ? [2, 2, 2] : [1, 1, 1],
+    x: isBig ? 3 : 0
+  })
+
   const color = isHovered ? 'indigo' : 'cornflowerblue'
 
   return (
-    <mesh
+    <a.mesh
       {...props}
+      ref={ref}
+      scale={size}
+      position-x={x}
       onClick={() => setIsBig(!isBig)}
       onPointerOut={() => setIsHovered(false)}
       onPointerOver={() => setIsHovered(true)}
     >
-      <boxBufferGeometry attach="geometry" args={[size, size, size]} />
-      <meshStandardMaterial attach="material" color={color} />
-    </mesh>
+      <sphereBufferGeometry attach="geometry" args={[1, 8, 6]} />
+      <meshPhongMaterial flatShading={true} shininess={100} attach="material" color={color} />
+    </a.mesh>
   )
 }
+
+// box args = [width, height, depth ]
+// sphere args = [radius (1), width min=3 (8), height min=2 (6)]
+// cylinder args = [radiusTop, radiusBottom, height, radialSegments]
 
 function Scene() {
   const { camera, gl: { domElement } } = useThree()
