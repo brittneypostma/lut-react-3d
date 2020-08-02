3da8d53078be71e47566ca4b3700817817427c0a
Brittney <brittney.postma@hotmail.com>
2020-08-01 20:36:52 -0400

    hooks and state to change cubes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import { Canvas } from 'react-three-fiber'
+import React, { useState } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Canvas, useThree, extend } from 'react-three-fiber'
 import './App.css';
 
-function Cube() {
+extend({ OrbitControls })
+
+function Cube(props) {
+  const [isBig, setIsBig] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const size = isBig ? 2 : 1
+  const color = isHovered ? 'indigo' : 'cornflowerblue'
+
   return (
-    <mesh position={[0, 0, 0]}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="blue" />
+    <mesh
+      {...props}
+      onClick={() => setIsBig(!isBig)}
+      onPointerOut={() => setIsHovered(false)}
+      onPointerOver={() => setIsHovered(true)}
+    >
+      <boxBufferGeometry attach="geometry" args={[size, size, size]} />
+      <meshStandardMaterial attach="material" color={color} />
     </mesh>
   )
 }
 
 function Scene() {
+  const { camera, gl: { domElement } } = useThree()
   return (
     <>
       <ambientLight />
-      <pointLight position={[-1, 2, 4]} />
+      <pointLight intensity={0.5} position={[-1, 2, 4]} />
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
+      <Cube rotation={[10, 20, 0]} position={[2, 2, 0]} />
+      <orbitControls args={[camera, domElement]} />
     </>
   )
 }
@@ -24,7 +42,6 @@ function App() {
   return (
     <Canvas>
       <Scene />
-      <Cube />
     </Canvas>
   );
 }
